b7df629b37860d119cea3f26abf5ca17762caca0
Dennis Mullen <dkmullen@users.noreply.github.com>
2017-05-29 19:29:51 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://localhost:27017/jwt-practice-app'
+
+};
+// "C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe"
